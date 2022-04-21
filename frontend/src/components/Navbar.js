83d83f50a9b874482fbc45ae19b53912cd97c8b0
Ankita Shanbhag <ankita.shanbhag@tuni.fi>
2022-04-21 10:27:19 +0300

    Navbar.js : Add code for material-ui.

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,9 +1,46 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import Teach from './Teach';
+import Home from './Home';
+import Learn from './Learn';
+import {AppBar, Typography, Toolbar, Tabs, Tab} from "@mui/material"
+import SchoolIcon from '@mui/icons-material/School';
+import '../styles/Navbar.css'
+
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+const pages = ['Home', 'Learn', 'Teach'];
 
 const Navbar = ()=>{
+    
     return(
         <>
+            <BrowserRouter>
+            
+            <AppBar sx={{background:"#063970"}}>
+                <Toolbar>
+                    <SchoolIcon fontSize="large"/>
+                    <Typography>
+                        Learn Languages
+                                          
+                    </Typography>
+                    
+                    <Tabs textColor="inherit">
+                        <Tab label="Home" component={NavLink} to={"/"}></Tab>
+                        <Tab label="Teach" component={NavLink} to={"/teach"}></Tab>
+                        <Tab label="Learn" component={NavLink} to={"/learn"}></Tab>                                              
+                    </Tabs>            
+                </Toolbar>
+            </AppBar>
+           
+            <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/teach" element={<Teach/>}></Route>
+          <Route path="/learn" element={<Learn/>}></Route>
+        </Routes>
+            </BrowserRouter>
+            
+        </>
+     /*   <>
             <nav>
                 <h2>Learn Languages</h2>
                 <ul>
@@ -24,7 +61,7 @@ const Navbar = ()=>{
                     </li>
                 </ul>
             </nav>
-        </>
+        </>*/
     )
 }
 
