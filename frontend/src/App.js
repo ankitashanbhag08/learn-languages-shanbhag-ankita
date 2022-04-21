c5330c8712c030d788aa4074e2db500b062be870
Ankita Shanbhag <ankita.shanbhag@tuni.fi>
2022-04-21 10:28:19 +0300

    App.js : Moved router-dom to Navbar.js.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import Teach from './components/Teach';
-import Home from './components/Home';
-import Learn from './components/Learn';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<Home/>}></Route>
-          <Route path="/teach" element={<Teach/>}></Route>
-          <Route path="/learn" element={<Learn/>}></Route>
-        </Routes>
-      </BrowserRouter>
+        <Navbar/>       
+      
     </div>
   );
 }
