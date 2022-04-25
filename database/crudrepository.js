18e7715ef81c8b546e40a3906cf88d1784960dd1
Ankita Shanbhag <ankita.shanbhag@tuni.fi>
2022-04-25 11:14:32 +0300

    crudrepository.js : Add update() for updating the records.

--- database/crudrepository.js
+++ database/crudrepository.js
@@ -38,7 +38,7 @@ let connectionFunctions = {
         
     },
     findAll: () =>{
-        let sql = "SELECT eng_word_master.id as eng_id,eng_word_master.word as eng_word,fin_word_master.id AS fin_id,fin_word_master.word as fin_word, category_master.name FROM eng_word_master" + 
+        let sql = "SELECT eng_word_master.id as eng_id,eng_word_master.word as eng_word,fin_word_master.id AS fin_id,fin_word_master.word as fin_word, category_master.name, category_master.id as cat_id FROM eng_word_master" + 
                     " JOIN word_meaning ON eng_word_master.id = word_meaning.eng_id"+
                     " JOIN fin_word_master ON fin_word_master.id = word_meaning.fin_id"+
                     " JOIN category_master ON eng_word_master.category_id = category_master.id;"  
@@ -64,7 +64,7 @@ let connectionFunctions = {
                 
                 let result3 = await insertData(sql3, [result1.insertId, result2.insertId])                
                 await connection.commit();
-                return("saved!")
+                return({engId:result1.insertId, finId:result2.insertId})
             }catch(err){
                 await connection.rollback();
                 console.log(err)
@@ -72,6 +72,45 @@ let connectionFunctions = {
             }   
     },
     
+    update: async(word)=>{
+         console.log(word)
+         let category = Number(word.category)
+         let finId = Number(word.engId)
+         let engId=Number(word.engId)
+          
+        let recordsUpdated = false
+        let sql1 = `UPDATE eng_word_master SET word = ?, category_id = ? WHERE id = ?`
+        let sql2 = `UPDATE fin_word_master SET word = ?, category_id = ? WHERE id = ?`
+        try{
+                await connection.beginTransaction();
+                console.log(word)
+                let result1 = await insertData(sql1, [word.engWord, category, engId])      
+                
+                let result2 = await insertData(sql2, [word.finWord, category, finId])
+                
+                //let result3 = await insertData(sql3, [result1.insertId, result2.insertId])                
+                await connection.commit();
+                recordsUpdated=true;
+                return(recordsUpdated)
+            }catch(err){
+                await connection.rollback();
+                console.log(err)
+                return("error!")                
+            }  
+        //try{
+           // await connection.beginTransaction()
+           /* Promise.all([insertData(sql1, [word.engWord, word.category, word.engId]), insertData(sql2, [word.finWord, word.category, word.finId])])
+            .then((data) => console.log("done"))
+            .catch((err)=>console.log(err));*/
+           // await connection.commit();
+            
+       // }catch(err){
+        //    await connection.rollback();
+         //   console.log(err)
+         //   return("error!")
+       // }
+    },
+
     close: (callback)=>{
         connection.end(()=>{
             console.log("Closing db")
